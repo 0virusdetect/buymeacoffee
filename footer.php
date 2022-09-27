26a3e54851d7ffdba2bdc607a78dda224ff0c121
Abhihsek Jaiswal <47599266+0virusdetect@users.noreply.github.com>
2022-09-27 14:41:20 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<script src="./bootstrap/js/bootstrap.js"></script>
+<script src="./script.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Buy Coffie</title>
+<link rel="icon" type="image/x-icon" href="./assets/cup.svg">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;500&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="./style.css">
+</head>
+<body onload="myFunction()">
+<div class="spinner">
+<span>L</span>
+<span>O</span>
+<span>A</span>
+<span>D</span>
+<span>I</span>
+<span>N</span>
+<span>G</span>
+</div>
